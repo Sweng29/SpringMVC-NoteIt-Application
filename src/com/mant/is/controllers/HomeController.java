bafdc442896a1306a171f12fe9daa38b5bf1bdcb
Sweng29 <khosokashif@gmail.com>
2019-06-12 00:11:41 +0500

    Added Models,

--- src/com/mant/is/controllers/HomeController.java
+++ src/com/mant/is/controllers/HomeController.java
@@ -1,25 +1,16 @@
 package com.mant.is.controllers;
 
-import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.RequestMapping;
 
-import com.mant.is.models.Employee;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 public class HomeController {
-
+	
 	@RequestMapping(value="/")
 	public String indexPage()
 	{
 		return "home";
 	}
-	
-	@RequestMapping(value="/employeeForm")
-	public String employeeForm(Model model)
-	{
-		model.addAttribute("employee",new Employee());
-		return "employee-form";
-	}
-	
+
 }
---
+++ src/com/mant/is/models/Note.java
@@ -0,0 +1,56 @@
+package com.mant.is.models;
+
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
+
+import javax.persistence.*;
+import java.util.Date;
+
+@Entity
+@Table(name = "notes")
+public class Note {
+
+    @Id
+    @GeneratedValue
+    private Long noteId;
+    private String note;
+    @CreatedDate
+    private Date createdDate;
+    @LastModifiedDate
+    private Date updatedDate;
+    @ManyToOne
+    @JoinColumn(name = "notebook_id")
+    private Notebook notebook;
+
+    public Long getNoteId() {
+        return noteId;
+    }
+
+    public void setNoteId(Long noteId) {
+        this.noteId = noteId;
+    }
+
+    public String getNote() {
+        return note;
+    }
+
+    public void setNote(String note) {
+        this.note = note;
+    }
+
+    public Date getCreatedDate() {
+        return createdDate;
+    }
+
+    public void setCreatedDate(Date createdDate) {
+        this.createdDate = createdDate;
+    }
+
+    public Date getUpdatedDate() {
+        return updatedDate;
+    }
+
+    public void setUpdatedDate(Date updatedDate) {
+        this.updatedDate = updatedDate;
+    }
+}
